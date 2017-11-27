7de8c52061e5041e8fd204485a56f077fcf1c903
fallen <haiyan.xu.vip@gmail.com>
2017-11-27 09:53:52 +0800

    * Get value fo global config

--- app/library/Helper/CommonTool.php
+++ app/library/Helper/CommonTool.php
@@ -4,6 +4,24 @@
  */
  namespace Helper;
 
- class CommonTool
- {
- }
+class CommonTool
+{
+    /**
+     * Get value of global config
+     * @param array $modules
+     * @param string $column
+     * @param string $defaultValue
+     * @return array|string
+     */
+    public static function config($modules = [], $column = '', $defaultValue = '')
+    {
+        if (empty($modules)) {
+            return $defaultValue;
+        }
+        $configs = getConfig($modules);
+        if (!empty($configs) && !empty($column)) {
+            return ArrayHelper::getValue($configs, $column, $defaultValue);
+        }
+        return $configs;
+    }
+}
