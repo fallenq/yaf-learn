e91cba8396240fad9fa559cb459acb344cc09f2a
fallen <haiyan.xu.vip@gmail.com>
2017-11-29 17:29:12 +0800

    * Get instance of service

--- app/modules/test/actions/index/IndexAction.php
+++ app/modules/test/actions/index/IndexAction.php
@@ -18,7 +18,7 @@ class IndexAction extends Action_Abstract
 //        dd(getConfig('common', 'test'));
 //        dd(TestDaoService::store([]));
 //        dd(TestDaoService::store(['name'=>"dd"]));
-//        dd(ArrayHelper::compareDifference(['b'=>2, 'c'=>3, 'd'=>4], ['a'=>1, 'b'=>2, 'c'=>3]));
+        dd(ArrayHelper::compareDifference(['b'=>2, 'c'=>3, 'd'=>4], ['a'=>1, 'b'=>2, 'c'=>3]));
 //        echo TimeHelper::formatTimestamp(null, 1, "-10 days");
 
 //        var_dump(ArrayHelper::getValue(['t1'=>['v1'=>['c1']]], 't1.v1', 0));
