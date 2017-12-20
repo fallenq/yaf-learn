6bec38e591824f49648051cc936c24e78b51cb89
fallen <haiyan.xu.vip@gmail.com>
2017-12-20 17:53:47 +0800

    *

--- app/library/Helper/TimeHelper.php
+++ app/library/Helper/TimeHelper.php
@@ -23,4 +23,26 @@ class TimeHelper
                 return date('Y-m-d H:i:s', $timestamp);
         }
     }
+
+    /**
+     * Get the last day of the month
+     * @param $year
+     * @param $month
+     * @return int
+     */
+    public static function getMonthLast($year, $month)
+    {
+        if (in_array($month, [1,3,5,7,8,10,12])) {
+            return 31;
+        } else if(in_array($month, [4,6,9,11])) {
+            return 30;
+        } else if($month == 2) {
+            if ($year % 400 == 0 || ($year % 4 == 0 && $year % 100 > 0)) {
+                return 29;
+            } else {
+                return 28;
+            }
+        }
+        return 0;
+    }
 }
