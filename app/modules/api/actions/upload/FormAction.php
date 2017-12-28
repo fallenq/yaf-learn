c50e46b9d3ef23c702756f022e533ae55c81494b
fallen <haiyan.xu.vip@gmail.com>
2017-12-28 19:00:17 +0800

    *

--- app/modules/api/actions/upload/FormAction.php
+++ app/modules/api/actions/upload/FormAction.php
@@ -9,8 +9,33 @@ class FormAction extends Action_Abstract
     public function execute () {
         $upload = new UploadTool();
         $fileInfo = $upload->processTempFileInfo();
-        $uploadFileUrl = $upload->store(ArrayHelper::getValue($fileInfo, 'file.tmp_name'), ArrayHelper::getValue($fileInfo, 'file.name'));
-        var_dump($uploadFileUrl);
+        if (empty($fileInfo)) {
+            if (count($fileInfo) == 1) {
+                $singleInfo = array_shift($fileInfo);
+                $singleUrl = $this->singleUpload($upload, $singleInfo);
+                var_dump($singleUrl);
+            } else {
+                $this->multiUpload($upload, $fileInfo);
+            }
+        } else {
+            var_dump('error');
+        }
         return false;
+    }
+
+    private function singleUpload($upload, $fileInfo)
+    {
+        return $upload->store(ArrayHelper::getValue($fileInfo, 'tmp_name'), ArrayHelper::getValue($fileInfo, 'name'));
+    }
+
+    private function multiUpload($upload, $fileInfo)
+    {
+        $outUrls = [];
+        foreach ($fileInfo as $item) {
+            if ($outUrl = $this->singleUpload($upload, $item)) {
+                $outUrls[] = $outUrl;
+            }
+        }
+        var_dump($outUrls);
     }
 }
